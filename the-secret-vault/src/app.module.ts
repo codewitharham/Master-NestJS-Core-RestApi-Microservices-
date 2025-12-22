59a543c642b97451929d9eb15306802f20f2b597
ArhamJamil <codewitharham.remote@gmail.com>
2025-12-22 18:00:39 +0500

    Guards&Interceptors Project - 12/22/2025 - 6:00PM

---
+++ the-secret-vault/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotesModuleModule } from './notes-module/notes-module.module';
+
+@Module({
+  imports: [NotesModuleModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
